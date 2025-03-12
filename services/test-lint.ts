81525d3ccc24b39ab6fa4fdbd1d5a829383946f7
Yasutaka Odo <yasutakaodo@gmail.com>
2025-03-12 14:46:28 +0900

    test: add test file for lint-staged with fixed ESLint error

---
+++ services/test-lint.ts
@@ -0,0 +1,8 @@
+// This is a test file to verify that the pre-commit hook works
+
+function testFunction() {
+  const testVar = 'test';
+  console.log('Test function', testVar);
+}
+
+export default testFunction;
