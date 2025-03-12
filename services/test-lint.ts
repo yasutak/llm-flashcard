ebd5bd435802bad2cb39f16ba810b6531621fa31
Yasutaka Odo <yasutakaodo@gmail.com>
2025-03-12 14:47:22 +0900

    chore: remove test file

--- services/test-lint.ts
+++
@@ -1,8 +0,0 @@
-// This is a test file to verify that the pre-commit hook works
-
-function testFunction() {
-  const testVar = 'test';
-  console.log('Test function', testVar);
-}
-
-export default testFunction;
